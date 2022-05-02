f16f8f69e25d6f4a9da47234341c850d0a06d508
el-rachc <101344803+el-rachc@users.noreply.github.com>
2022-05-02 21:16:46 +0100

    Delete scripts directory

--- scripts/my-scripts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p analysis docs data
-for my_directory in scripts analysis docs data;do
-  touch ${my_directory}/README.md
-  echo "# ${my_directory}" >> ${my_directory}/README.md
-done
-
